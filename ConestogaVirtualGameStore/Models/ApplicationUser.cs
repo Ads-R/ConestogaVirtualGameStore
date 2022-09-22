a92ff2fcbff43b03c2ec46404ea6bc30ccee4e1b
Adriel Roque <adss.roque@gmail.com>
2022-09-22 17:41:44 -0400

    Added DbContext and AppUser class

---
+++ ConestogaVirtualGameStore/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ConestogaVirtualGameStore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ ConestogaVirtualGameStore/Models/GameStoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConestogaVirtualGameStore.Models
+{
+    public class GameStoreContext : IdentityDbContext<ApplicationUser>
+    {
+        public GameStoreContext(DbContextOptions<GameStoreContext> options): base(options)
+        {
+        }
+    }
+}
