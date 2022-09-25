d8b1f4540a004d04f48174cfe50127d1f55b6f2e
Adriel Roque <adss.roque@gmail.com>
2022-09-25 14:34:23 -0400

    Added UserController with Register get and View

---
+++ ConestogaVirtualGameStore/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConestogaVirtualGameStore.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConestogaVirtualGameStore/Models/RegisterModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConestogaVirtualGameStore.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        [Compare("Password", ErrorMessage = "Password must match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
