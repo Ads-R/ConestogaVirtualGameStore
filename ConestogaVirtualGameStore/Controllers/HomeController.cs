c985e76450e9845bcad061e16a55ee1c630dcc00
Adriel Roque <adss.roque@gmail.com>
2022-10-06 17:57:02 -0400

    Updated Index page

--- ConestogaVirtualGameStore/Controllers/HomeController.cs
+++ ConestogaVirtualGameStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using ConestogaVirtualGameStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +13,34 @@ namespace ConestogaVirtualGameStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly GameStoreContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, GameStoreContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Games.ToListAsync());
+        }
+
+        public async Task<IActionResult> GameDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gameModel = await _context.Games
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gameModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(gameModel);
         }
 
         public IActionResult Privacy()
