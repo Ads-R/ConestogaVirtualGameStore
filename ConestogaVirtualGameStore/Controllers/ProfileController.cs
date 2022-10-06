5102e6d894feff2e7dbe024abe2fbb9fae4b8391
Adriel Roque <adss.roque@gmail.com>
2022-10-06 15:15:05 -0400

    Updated Preferences
    Combined with profile controller

--- ConestogaVirtualGameStore/Controllers/ProfileController.cs
+++ ConestogaVirtualGameStore/Controllers/ProfileController.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ConestogaVirtualGameStore.Controllers
@@ -69,6 +71,63 @@ namespace ConestogaVirtualGameStore.Controllers
                 return RedirectToAction("Index", "Profile");
             }
             return View("UpdateProfile", profile);
+        }
+
+        public async Task<IActionResult> UpdatePreference()
+        {
+            ApplicationUser user = await userManager.GetUserAsync(User);
+            var pref = await _context.Preferences.FirstOrDefaultAsync(a => a.UserId == user.Id);
+            if (pref != null)
+            {
+                PreferenceViewModel preference = new PreferenceViewModel
+                {
+                    PreferencesModelId = pref.PreferencesModelId,
+                    UserId = pref.UserId,
+                    Category = pref.Category != null ? pref.Category.Split(",") : null,
+                    Platform = pref.Platform != null ? pref.Platform.Split(",") : null
+                };
+                return View(preference);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdatePreference([Bind("PreferencesModelId, UserId, Platform, Category")] PreferenceViewModel preference)
+        {
+            ApplicationUser user = await userManager.GetUserAsync(User);
+            PreferencesModel pref = await _context.Preferences.Where(a => a.UserId == user.Id).FirstOrDefaultAsync();
+            if (pref.UserId != preference.UserId && pref.PreferencesModelId != preference.PreferencesModelId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+
+                pref.Category = preference.Category != null ? preference.Category.Join(",") : null;
+                pref.Platform = preference.Platform != null ? preference.Platform.Join(",") : null;
+                _context.Update(pref);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Profile");
+            }
+            /* var id = _context.Preferences.Where(a => a.UserId == user.Id).AsNoTracking().FirstOrDefault();
+            if(user.Id != preference.UserId && id.PreferencesModelId != preference.PreferencesModelId)
+            {
+                return NotFound();
+            } 
+            if (ModelState.IsValid)
+            {
+                PreferencesModel pref = new PreferencesModel
+                {
+                    PreferencesModelId = preference.PreferencesModelId,
+                    UserId = preference.UserId,
+                    Category = preference.Category != null ? preference.Category.Join(",") : null,
+                    Platform = preference.Platform != null ? preference.Platform.Join(",") : null
+                }; 
+                _context.Update(pref);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home"); 
+            } */
+            return View("UpdatePreference", preference);
         }
 
         public bool IsDateValid(DateTime dob)
