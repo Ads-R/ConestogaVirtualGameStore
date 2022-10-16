e4b14b574be38135da2faeae50bb2e9b63780b3c
Dong Yan <anne.dyan@gmail.com>
2022-10-16 16:40:18 -0400

    updated the game page labels and event page  labels
    update labels

--- ConestogaVirtualGameStore/Models/EventModel.cs
+++ ConestogaVirtualGameStore/Models/EventModel.cs
@@ -6,14 +6,18 @@ namespace ConestogaVirtualGameStore.Models
     public class EventModel
     {
         public int EventModelId { get; set; }
+        [Display(Name ="Event Name")]
         [Required]
         public string EventName { get; set; }
+        [Display(Name = "Event Description")]
         public string EventDescription { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dddd, MMMM d, yyyy}")]
+        [Display(Name = "Event Start Date")]
         public DateTime EventStartDate { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dddd, MMMM d, yyyy}")]
+        [Display(Name = "Event End Date")]
         public DateTime EventEndDate { get; set; }
     }
 }
