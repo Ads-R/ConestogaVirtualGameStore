86beafe0e2c23a9a2684fd0a7836bf0699ae1654
Adriel Roque <adss.roque@gmail.com>
2022-11-10 01:21:52 -0500

    Added Friends/FriendsList feature

---
+++ ConestogaVirtualGameStore/Models/ViewModels/FriendSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ConestogaVirtualGameStore.Models.ViewModels
+{
+    public class FriendSearchViewModel
+    {
+        public string UserId { get; set; }
+        public List<ApplicationUser>? Users { get; set; }
+        public List<string>? AllFriends { get; set; }
+        public List<ApplicationUser>? AcceptedFriends { get; set; }
+        public List<ApplicationUser>? PendingApproval { get; set; }
+        public List<ApplicationUser>? PendingRequest { get; set; }
+    }
+}
---
+++ ConestogaVirtualGameStore/Services/IFriendService.cs
@@ -0,0 +1,23 @@
+﻿using ConestogaVirtualGameStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConestogaVirtualGameStore.Services
+{
+    public interface IFriendService
+    {
+        Task<IEnumerable<string>> GetAllFriends(string userId);
+        Task<IEnumerable<ApplicationUser>> GetAllAcceptedFriends(string userId);
+        Task<IEnumerable<ApplicationUser>> GetPendingApproval(string userId);
+        Task<IEnumerable<ApplicationUser>> GetPendingRequest(string userId);
+
+        IQueryable<ApplicationUser> SearchUsers(string searchString, string userName);
+        void AddFriend(string userId, string friendId);
+        void DeleteFriend(string userId, string friendId);
+        void AcceptRequest(string userId, string friendId);
+        void DeclineRequest(string userId, string friendId);
+        void CancelRequest(string userId, string friendId);
+
+    }
+}
